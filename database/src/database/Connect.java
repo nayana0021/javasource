2ee7b1e3ded2b93e0528f939d6b6d32ac3ee98f4
nayana0021 <qazzxx00@gmail.com>
2023-03-21 22:20:07 +0900

    jdbc 프로그램 작성

---
+++ database/src/database/Connect.java
@@ -0,0 +1,52 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ *  JDBC(Java Database Connectivity)
+ *  자바에서 데이터베이스에 접속할 수 있도록 제공해주는 자바 api
+ *  
+ *  프로젝트에 ojdbc8.jar 포함되어야 함
+ *  [build path] - [configure build path] - class path 에 ojdbc8.jar 포함
+ *  
+ *  1) connection 가져오기
+ *  	-드라이버 로드
+ *  	-연결문자열 연결 시도
+ */
+
+public class Connect {
+	public static void main(String[] args) {
+		Connection con = null;
+		
+		try {
+			Class.forName("oracle.jdbc.OracleDriver"); //드라이버 이름 (정해져있음)
+			
+			// jdbc:oracle:thin:@ 고정
+			
+			// localhost:1521:xe => 변경 가능한 값
+			// localhost == 127.0.0.1 (내컴퓨터)
+			// 1521 (기본포트번호)
+			// xe(기본 sid 이름) 
+			
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "scott";
+			String password = "TIGER";
+			
+			con = DriverManager.getConnection(url, user, password);
+			
+			if(con!=null) {
+				System.out.println("연결되었습니다."); //con 이 null이면 null pointer exception이 나겠지
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+		
+		
+		
+	}
+
+}
